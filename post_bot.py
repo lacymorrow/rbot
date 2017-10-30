9926ecbb76f1f1780ab00156c2750147b6829e62
Lacy Morrow <me@lacymorrow.com>
2017-10-30 08:07:23 -0700

    updating; don't use top posts

--- post_bot.py
+++ post_bot.py
@@ -52,23 +52,29 @@ SEND_FLAG = True
 SORT_DATE = 'month' # 'week', 'month', etc. via reddit
 POST_DELAY = 10 # seconds ; 600 === 10 mins
 SEARCH_LIMIT = 50 # Posts within subs that  are searched Max 1000
-AGE_THRESHOLD = 30 # Default 365 days (considered a month)
+AGE_THRESHOLD = 25 # Default 25 days (considered a month); 20-25 is active
 EXECUTION_TOTAL = 500 # TOTAL APPLICATION RUN COUNT
-BANNED_WORDS = ['[OC]', 'hello']
+BANNED_WORDS = ['[OC]', 'OC', 'test']
+
+# PERSONAL_SUBS = ['test', 'me_irl', 'lgbqt',] # 'askreddit', 'askwomen', 'askmen'] 
 
 # ACTIVE_SUBS = ['test']
-ACTIVE_SUBS = subs.most_subscribed_subs # ['test', 'me_irl', 'lgbqt',] # 'askreddit', 'askwomen', 'askmen'] 
+ACTIVE_SUBS = subs.most_subscribed_subs
+
+# NEWS SUBS
+NEWS_SUBS = ['usanews', 'worldnews', 'poitics', 'neutralpolitics', 'unceensorednews']
 
 # 'Futurology' = 5 day old 
 # 'technology' = 5 daays old
 # 'youtubehaiku' = 5 daays old
-BANNED_SUBS = ['gifs', 'bodyweightfitness', 'overwatch', 'hearthstone', 'tattoos', 'pokemon', 'futurology', 'technology', 'youtubehaiku']
+BANNED_SUBS = ['pics', 'gifs', 'nosleep', 'bodyweightfitness', 'overwatch', 'hearthstone', 'tattoos', 'pokemon', 'futurology', 'technology', 'worldnews', 'youtubehaiku']
 
-preface = "I know it's been posted before but... "
+preface = ''
 
 
 # Skip banned subs
-working_subs = [x for x in ACTIVE_SUBS if x not in BANNED_SUBS]
+working_active_subs = [x for x in ACTIVE_SUBS if x not in BANNED_SUBS]
+working_news_subs = [x for x in NEWS_SUBS if x not in BANNED_SUBS]
 
 
 # Other Subs to consider to widen the net
@@ -97,12 +103,12 @@ def run_bot(r):
     
     # random.choice(ACTIVE_SUBS) # number is how far from top post to use (0 is default/top)
     ## Todo, active sub may be repeated/ not removed
-    active_sub = random.choice(ACTIVE_SUBS) 
-        
-    # print 'Queue: ' + str(ACTIVE_SUBS)
-    print 'TARGETING RANDOM SUB: ' + active_sub
+    active_sub = random.choice(working_active_subs) 
+    news_sub = random.choice(working_news_subs)
 
-
+    print '\n RUNNING #1 \n'
+    print 'TARGETING ACTIVE SUB: ' + active_sub
+    print 'TARGETING NEWS SUB: ' + news_sub
 
     # get top OLD posts from working subs (default ALL TIME) (POSTS that stood test of time)
     posts = [] # posts old enough to consider for reposting
@@ -114,11 +120,18 @@ def run_bot(r):
             post.age = datetime.date.today() - datetime.date.fromtimestamp(post.created)
             # print max(post.age.days, key=lambda item: item[1])
             # check if post is old enough to warrant a repost (don't repost brand new posts, better for xposts)
-            
             if(int(post.age.days) > AGE_THRESHOLD):
                 post = restyle_post(post)
                 posts.append(post)
-    # if posts are old enough, create repost
+
+
+    # # # repost top stories immediately
+    # news_posts = r.subreddit(news_sub).new()
+    # for post in news_posts:
+    #     if post.shortlink:
+    #         post = restyle_post(post)
+    #         posts.append(post)
+
     # optionally, randomize;
     create_posts(r, posts[:-1])
 
@@ -167,11 +180,24 @@ def create_posts(r, posts):
         except praw.exceptions.APIException as e:
             s = str(e)
             print "APIException PRAW: " + s
+            if "SUBREDDIT_NOTALLOWED" in s:
+                print "Private Sub"
+                coninue;
+            next_post_delay = int(filter(str.isdigit, s))
+            if not str(type(next_post_delay) is int):
+                exit(1)
+            sleep(next_post_delay)
+
+        except APIException.PRAW as e:
+            s = str(e)
+            print "APIException PRAW: " + s
+            if "SUBREDDIT_NOTALLOWED" in s:
+                print "Private Sub"
+                coninue;
             next_post_delay = int(filter(str.isdigit, s))
             if not str(next_post_delay.isdigit()):
                 exit(1)
             sleep(next_post_delay)
-            continue;
 
         except AttributeError as e:
             print "AttributeError: " + str(e)
@@ -179,6 +205,9 @@ def create_posts(r, posts):
 
         except AssertionError as e:
             print "AssertionError: " + str(e)
+            exit(1)
+        except Exception as e:
+            print "Error" + e
             exit(1)
         except:
             print "Uncaught Error"
